8ed5a0bdfd97eac7478fa412d34a9ef8697bc20f
James Lawrence <jljatone@gmail.com>
2024-09-08 20:59:27 -0400

    fix up example

--- examples/provider-install-verfication/main.tf
+++ examples/provider-install-verfication/main.tf
@@ -1,16 +1,19 @@
 terraform {
   required_providers {
-    eg = "hashicorp.com/edu/eg"
+    eg = {
+      source  = "egdaemon/eg"
+      version = "0.0.5"
+    }
   }
 }
 
-provider "eg" {}
+# provider "eg" {}
 
 locals {
   exampledir = "${path.module}/rootfs"
 }
 
-resource "eg_tar" "archive" {
+resource "eg_tar" "default" {
   dynamic "source" {
     for_each = sort(toset(fileset(local.exampledir, "*")))
     content {
@@ -23,4 +26,11 @@ resource "eg_tar" "archive" {
     base64   = base64encode("waaaaaaaaaaaaaaaaaaaaaaaaat")
     location = "example.3.txt"
   }
+}
+
+resource "local_file" "default" {
+  content_base64       = eg_tar.default.archiveb64
+  filename             = "example.tar.gz"
+  file_permission      = "0600"
+  directory_permission = "0750"
 }
